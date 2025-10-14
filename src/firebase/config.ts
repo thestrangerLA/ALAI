01c88974c42b4b80cd4447d34a99e4e09c867550
Work Laohugtravel <laohugtravelwork@gmail.com>
2025-10-14 02:31:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7907390526-508e3",
+  "appId": "1:591025345708:web:3ecf0024f008996d8abb09",
+  "apiKey": "AIzaSyDG3Xlfzln_FqyKwAHGTVhlMtjz1VnLzA4",
+  "authDomain": "studio-7907390526-508e3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "591025345708"
+};
