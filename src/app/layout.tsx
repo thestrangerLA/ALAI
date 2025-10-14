33a7d0f885a30e3b810b3df06a6e775d1b36fe6d
Work Laohugtravel <laohugtravelwork@gmail.com>
2025-10-14 09:13:25 +0000

    change web to modern font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
+
 import type { Metadata } from "next";
-import { Noto_Sans_Lao } from "next/font/google";
+import { Prompt } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { FirebaseClientProvider } from "@/firebase/client-provider";
 
-const fontSans = Noto_Sans_Lao({
-  subsets: ["lao"],
+const fontSans = Prompt({
+  subsets: ["thai", "latin"],
   variable: "--font-sans",
   weight: ["400", "700"],
 });
