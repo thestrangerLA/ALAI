f53d79dca7d4e7a1a8371a0b423d2024d7ce05d4
Work Laohugtravel <laohugtravelwork@gmail.com>
2026-03-10 02:21:18 +0000

    delete all code then i will put new code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,13 @@
-
-import type { Metadata } from "next";
-import { Noto_Sans_Lao } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-import { FirebaseClientProvider } from "@/firebase/client-provider";
-
-const fontSansLao = Noto_Sans_Lao({
-  subsets: ["lao", "latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: '--font-sans-lao',
-});
-
-export const metadata: Metadata = {
-  title: "POS ລະບົບຂາຍສິ້ນສ່ວນ",
-  description: "ລະບົບຂາຍສິ້ນສ່ວນລົດຍົນ",
-};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="lo" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSansLao.variable
-        )}
-      >
-        <FirebaseClientProvider>{children}</FirebaseClientProvider>
-      </body>
+    <html lang="lo">
+      <body>{children}</body>
     </html>
   );
 }
